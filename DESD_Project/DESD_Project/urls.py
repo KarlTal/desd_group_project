aeec2f2e6c9ede47a1f371947e6f6008679e238c
KarlTal <talaverakarl04@gmail.com>
2023-03-15 13:55:02 +0000

    Created a cinemaManager app

--- DESD_Project/DESD_Project/urls.py
+++ DESD_Project/DESD_Project/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('UWEFlix.urls')),
+    path('account/CinemaManager/', include('CinemaManager.urls')),
 ]
