21240c2edd782d5c81aed85ce8777547997eeb7d
AlexR <alex2.roussel-smith@live.uwe.ac.uk>
2023-03-30 15:26:32 +0100

    impl credit debiting

--- DESD_Project/BookingManager/views.py
+++ DESD_Project/BookingManager/views.py
@@ -19,24 +19,29 @@ def book_film(request, film_id, showing_id):
         showing = Showing.objects.get(id=showing_id)
         club = None
 
-        if request.user.is_authenticated:
+        # Wrap with try catch as if the user is not logged in (e.g. a Customer) then this will error.
+        try:
             profile = UserProfile.objects.get(user_obj=request.user)
+        except SomeModel.DoesNotExist:
+            profile = None
+
+        if profile is not None:
             club = profile.club
 
         remaining_seats = showing.screen.capacity - showing.seats_taken
 
         if request.POST:
+
+            if request.user.is_anonymous:
+                # TODO: Add 'payment processing' page for Customers
+                return redirect('/')
+
             student_price = showing.student_cost
             adult_price = showing.adult_cost
             child_price = showing.child_cost
 
             if club is not None:
                 student_price = student_price * decimal.Decimal((1 - (club.discount / 100)))
-
-            if request.user.is_authenticated:
-                user_email = request.user.email
-            else:
-                user_email = request.POST["user_email"]
 
             student_quantity = int(request.POST["student_quantity"])
             adult_quantity = int(request.POST["adult_quantity"])
@@ -51,20 +56,27 @@ def book_film(request, film_id, showing_id):
                 total_price = float((adult_quantity * adult_price) + (child_quantity * child_price) + (
                         student_quantity * student_price))
 
-                new_booking = Booking.objects.create(user_email=user_email, showing=showing, date=date.today(),
-                                                     total_price=total_price, ticket_count=total_quantity)
+                if total_price > profile.credits:
+                    error_message = "You do not have sufficient credit for this purchase!"
+                else:
+                    # Update the users credit.
+                    profile.credits = profile.credits - total_price
+                    profile.save()
 
-                for i in range(adult_quantity):
-                    Ticket.objects.create(booking=new_booking, ticket_type="Adult", price=adult_price)
-                for i in range(child_quantity):
-                    Ticket.objects.create(booking=new_booking, ticket_type="Child", price=child_price)
-                for i in range(student_quantity):
-                    Ticket.objects.create(booking=new_booking, ticket_type="Student", price=student_price)
+                    new_booking = Booking.objects.create(user_email=request.user.email, showing=showing, date=date.today(),
+                                                         total_price=total_price, ticket_count=total_quantity)
 
-                showing.seats_taken += total_quantity
-                showing.save()
+                    for i in range(adult_quantity):
+                        Ticket.objects.create(booking=new_booking, ticket_type="Adult", price=adult_price)
+                    for i in range(child_quantity):
+                        Ticket.objects.create(booking=new_booking, ticket_type="Child", price=child_price)
+                    for i in range(student_quantity):
+                        Ticket.objects.create(booking=new_booking, ticket_type="Student", price=student_price)
 
-                return redirect(home)
+                    showing.seats_taken += total_quantity
+                    showing.save()
+
+                    return redirect(home)
 
         return render(request, 'BookingManager/book_film.html',
                       {'film': lookup, 'club': club, 'showing': showing, 'remaining_seats': remaining_seats,
