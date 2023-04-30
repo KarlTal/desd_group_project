de5c8cd13f81a4d2a87944a71dc7e884b4896cfe
AlexR <alex2.roussel-smith@live.uwe.ac.uk>
2023-04-30 17:01:20 +0100

    .

--- DESD_Project/UWEFlix/models.py
+++ DESD_Project/UWEFlix/models.py
@@ -95,7 +95,7 @@ class Film(models.Model):
     age_rating = models.CharField(max_length=5, choices=RATINGS)
     duration = models.IntegerField()
     description = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='films/', null=True)
+    image = models.ImageField(upload_to='Assets/', null=True)
 
     def __str__(self):
         return self.title
