4a211fb3fe1f38228eed28150fc9420c74ba9f7a
adrianmitrea0209 <122695879+adrianmitrea0209@users.noreply.github.com>
2023-05-01 16:14:30 +0100

    Simple commit

--- DESD_Project/DESD_Project-entrypoint.sh
+++ DESD_Project/DESD_Project-entrypoint.sh
@@ -7,5 +7,5 @@ echo "Waiting for MySQL Database Service to start..."
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py loaddata initial_data.json
+python manage.py loaddata initial_data_2.json
 python manage.py runserver 0.0.0.0:8000
