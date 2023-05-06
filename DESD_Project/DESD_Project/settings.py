faba82b25867d106ebf0cf56642b7288351d5519
AlexR <alex2.roussel-smith@live.uwe.ac.uk>
2023-05-06 02:42:12 +0100

    .

--- DESD_Project/DESD_Project/settings.py
+++ DESD_Project/DESD_Project/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'DESD_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'desd-project-db',
-#         'USER': 'new',
-#         'PASSWORD': '123',
-#         'HOST': 'db',
-#         'PORT': '3306',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'desd-project-db',
+        'USER': 'new',
+        'PASSWORD': '123',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 LOGOUT_REDIRECT_URL = 'login/'
 LOGIN_URL = 'login/'
