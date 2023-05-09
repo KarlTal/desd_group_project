f875e4664d6bd0b618d27f7990364bc731e44d22
KarlTal <talaverakarl04@gmail.com>
2023-05-09 01:03:07 +0100

    fixed booking cancellation and timezone stuff

--- DESD_Project/DESD_Project/settings.py
+++ DESD_Project/DESD_Project/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'DESD_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'desd-project-db',
-        'USER': 'new',
-        'PASSWORD': '123',
-        'HOST': 'db',
-        'PORT': '3306',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'desd-project-db',
+#         'USER': 'new',
+#         'PASSWORD': '123',
+#         'HOST': 'db',
+#         'PORT': '3306',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 LOGOUT_REDIRECT_URL = 'login/'
 LOGIN_URL = 'login/'
@@ -131,7 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
